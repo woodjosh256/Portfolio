2ded7f86d33273ff78795832c5eaed66f8c28325
joshwood <wood.josh256@gmail.com>
2023-10-02 11:01:04 -0500

    Changed name Vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/woodjosh256.github.io",
+  base: "/Portfolio",
   plugins: [react()],
 })
